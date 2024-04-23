112e5b2ec48405163249db35e9398b12c7a05b9b
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-04-23 20:54:15 +0800

    added backslash

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import Unfonts from "unplugin-fonts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-react-js',
+  base: '/vite-react-js/',
   plugins: [
     react(),
     Pages(),
